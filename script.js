bbdd459d473d8b456e3612637fb051fb2bdb5547
pradeep thirvaskur <142670238+PradeepThirvaskur@users.noreply.github.com>
2024-07-24 21:53:48 +0530

    created a submission

--- script.js
+++
@@ -1,7 +0,0 @@
-// your code here
-document.getElementById('button').addEventListener('click', function(event) {
-  event.preventDefault(); // Prevent the form from submitting normally
-  var name = document.getElementById('name').value;
-  var year = document.getElementById('year').value;
-  document.getElementById('url').textContent = "https://localhost:8080/?name=" + name + "&year=" + year;
-});
